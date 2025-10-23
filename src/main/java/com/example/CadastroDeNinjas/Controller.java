343ed95892edec2990f9a82bf62c08279e0f9e33
Arthur Godinho <arthurgodinho155@gmail.com>
2025-10-23 11:45:29 -0300

    segundo commit

---
+++ src/main/java/com/example/CadastroDeNinjas/Controller.java
@@ -0,0 +1,15 @@
+package com.example.CadastroDeNinjas;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/BoasVindas")
+    public String BoasVindas() {
+        return "Olá! Boas Vindas!";
+    }
+}
