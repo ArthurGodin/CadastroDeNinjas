cc6bccbae05c48bf485c898e1517eef9db88fd9b
Arthur Godinho <arthurgodinho155@gmail.com>
2025-10-23 17:43:43 -0300

    123123

---
+++ src/main/java/com/example/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -0,0 +1,28 @@
+package com.example.CadastroDeNinjas.Missoes;
+
+import com.example.CadastroDeNinjas.Ninjas.NinjaModel;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class MissoesModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String dificuldade;
+
+    @OneToMany(mappedBy = "missoes")
+    private List<NinjaModel> ninjas;
+
+
+
+}
--- src/main/java/com/example/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/com/example/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -1,4 +1,4 @@
-package com.example.CadastroDeNinjas;
+package com.example.CadastroDeNinjas.Ninjas;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
